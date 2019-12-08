9f7833c3fc1595ad25cc5570584406ecc1191a62
Thomas Schiller <thomas.f.schiller@gmail.com>
2019-12-08 17:02:00 +0100

    Read in household asset/debt data

--- AssetPurchases.R
+++ AssetPurchases.R
@@ -3,28 +3,34 @@
 # Thomas Schiller 
 
 
-# Prepare working space ################## 
+## Prepare working space
 rm(list = ls())
 gc()
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # Load packages
-toload <- c("ggplot2", "ggfortify","forecast","tidyverse","stargazer", "lodown")
+toload <- c("ggplot2", "ggfortify","forecast","tidyverse","stargazer", "lodown", "readxl")
 
 lapply(toload,require, character.only = T)
 
 
 ## Construct Household Balance Sheet for First and Fifth Quintiles (following Domanski et al 2016)
 
-# Read in data
-data <- lapply(paste0("Data/",list.files("Data")), function(x) read.csv(x)[,2])
-data <- lapply(data, as.ts)
+financialAsset<-read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 6 16", skip=88)
+financialAssets<-financialAsset[-c(1:6,13:84),]
+colnames(financialAssets)[1]<-"Percentile of income"
 
-myfunction3<-function(name){
-  variable<-deparse(substitute(name))
-  assign(variable, read.csv2(file = paste0(substitute(name), ".csv"), sep = ","), env =.GlobalEnv)
-}
+stockHolding<-read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 7")
+stockHoldings<-stockHolding[-c(1:7,14:26),-c(1:20,22:31)]
+colnames(stockHoldings)[1]<-"Percentile of income"
 
+nonfinancialAsset<-read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 9 16", skip=2)
+nonfinancialAssets<-nonfinancialAsset[-c(1:92, 99:170),]
+colnames(nonfinancialAssets)[1]<-"Percentile of income"
 
+headers <- read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 13 16 Alt", range="A90:K91", col_names = FALSE)
+headersDebt <- sapply(headers,paste,collapse="_")
+debt<-read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 13 16 Alt", range="A98:K103", col_names = FALSE)
+names(debt)<-headersDebt
 
 
