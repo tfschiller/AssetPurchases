6051fbea421afc8008235c43fb8369735e1833a1
Thomas Schiller <thomas.f.schiller@gmail.com>
2019-12-08 15:45:52 +0100

    Begin reading in SCF data

--- AssetPurchases.R
+++ AssetPurchases.R
@@ -1,13 +1,30 @@
-##################  prepare working space ################## 
+# Software Engineering for Economists
+# The Effect of Asset Purchases on Asset Prices and Inequality
+# Thomas Schiller 
+
+
+# Prepare working space ################## 
 rm(list = ls())
 gc()
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-#load packages
-toload <- c("ggplot2", "ggfortify","forecast","magrittr","stargazer")
+# Load packages
+toload <- c("ggplot2", "ggfortify","forecast","tidyverse","stargazer", "lodown")
 
 lapply(toload,require, character.only = T)
 
-#read in data
+
+## Construct Household Balance Sheet for First and Fifth Quintiles (following Domanski et al 2016)
+
+# Read in data
 data <- lapply(paste0("Data/",list.files("Data")), function(x) read.csv(x)[,2])
 data <- lapply(data, as.ts)
+
+myfunction3<-function(name){
+  variable<-deparse(substitute(name))
+  assign(variable, read.csv2(file = paste0(substitute(name), ".csv"), sep = ","), env =.GlobalEnv)
+}
+
+
+
+
