f3ac232e413174073de8f65eeb5076953ff1b636
Thomas Schiller <thomas.f.schiller@gmail.com>
2019-12-29 21:21:44 +0000

    Added household debt table

--- AP/Inequality.R
+++ AP/Inequality.R
@@ -29,20 +29,18 @@ nonfinancialAssets<-nonfinancialAsset[10:15,]
 colnames(nonfinancialAssets)[1]<-"Percentile of income"
 colnames(nonfinancialAssets)[2:9]<-read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 9 16 means", range = "B3:I3", col_names = FALSE)
 
-
 # Total Assets
 householdPortfolio<-Reduce(function(x, y) left_join(x, y, by="Percentile of income"), list(financialAssets,nonfinancialAssets))
 colnames(householdPortfolio)[c(11,18)]<-c("Other financial assets", "Other nonfinancial assets")
 householdPortfolio[,2:20]<-as.numeric(unlist(householdPortfolio[,2:20]))
 
+# Combine the fifth and sixth rows as these represent the eighth and ninth deciles 
+householdPortfolio[5,2:20]<-householdPortfolio[5,2:20]+householdPortfolio[6,2:20]
+householdPortfolio<-householdPortfolio[-6,]
+householdPortfolio[c(1,5),1]<-c("0-19.9", "80-100")
 
 
-
-
-
-
-
-row.names(householdPortfolio)<-householdPortfolio[,1]
+row.names(householdPortfolio)<-unlist((householdPortfolio[,1]))
 householdPortfolio[is.na(householdPortfolio)]<-0
 
 
@@ -55,37 +53,24 @@ colnames(householdassets)<-colnames(householdPortfolio[2:20])
   
 
 # Debt 
-headers <- read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 13 16 Alt means", range="B3:K4", col_names = FALSE)
-headersDebt <- sapply(headers,paste,collapse="_")
 debt<-read_excel("scf2016_tables_internal_nominal_historical.xlsx", sheet="Table 13 16 Alt means", range="A12:K17", col_names = FALSE)
-names(debt)<-headersDebt
-colnames(debt)[1]<-"Percentile of income"
-
-
-
-
-
-
-
-
-colnames(householdPortfolio)[c(11,19,23,24,25,26,28,29,30,31)]<-c("Other financial assets", "Other nonfinancial assets", 
-                                                                  "HELOC - Secured by primary residence", "Other residential debt", 
-                                                                  "Credit card balances", "Lines of credit not secured by residential property",
+colnames(debt)[2:length(debt)]<-c("Mortgages - Secured by primary residence", "HELOC - Secured by primary residence", "Other residential debt", 
+                                                                  "Credit card balances", "Lines of credit not secured by residential property", "Education loans",
                                                                   "Vehicle loans", "Other installment loans", "Other debt", "Any debt")
-householdPortfolio[1:3,5]<-0
-householdPortfolio[,2:31]<-as.numeric(unlist(householdPortfolio[,2:31]))
-
-
-# Calculate relative weights of each asset class for the different income quintiles (as percentage of total asset holdings)
-
-pcts <- as.data.frame(lapply(householdPortfolio[,-1], function(x) {
-  x / apply(householdPortfolio[,-1],1,sum)
-}))*100
+rownames(debt)<-unlist(debt[,1])
 
 
 # Combine the fifth and sixth rows as these represent the eighth and ninth deciles 
-pcts1<-pcts[-6,]
-pcts1[5,]<-pcts[5,]+pcts[6,]
+debt[5,2:11]<-debt[5,2:11]+debt[6,2:11]
+debt<-debt[-6,]
+debt[c(1,5),1]<-c("0-19.9", "80-100")
+
+# Calculate percent of household portfolio for each asset type by income distrbution
+householddebt <- as.data.frame(lapply(debt[,-1], function(x) {
+  x / apply(debt[,-1],1,sum)}))*100
+
+rownames(householddebt)<-unlist(debt[,1])
+colnames(householddebt)<-colnames(debt[2:11])
 
 
 # Keep only the asset class a la Domanski et al 2016
